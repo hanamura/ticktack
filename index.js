392549ff5a1e603d0f86c59dd26d4858c7293058
Taro Hanamura <m@hanamurataro.com>
2016-12-22 20:12:34 +0900

    Update to ES6

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
-var EventEmitter = require('eventemitter2').EventEmitter2;
+'use strict'
 
-function Tick() {
-  EventEmitter.apply(this, Array.prototype.slice.apply(arguments));
-  try {
-    this._request = (
-      window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.oRequestAnimationFrame ||
-      window.msRequestAnimationFrame
-    ).bind(window);
-    this._cancel = (
-      window.cancelAnimationFrame ||
-      window.webkitCancelAnimationFrame ||
-      window.mozCancelAnimationFrame ||
-      window.oCancelAnimationFrame ||
-      window.msCancelAnimationFrame
-    ).bind(window);
-    this._raf = true;
-  } catch (e) {
-    this._request = function(f) { return setTimeout(f, 1000 / 60) };
-    this._cancel  = function(i) { clearTimeout(i) };
-    this._raf     = false;
+const EventEmitter = require('eventemitter2').EventEmitter2
+
+let tick = null;
+
+module.exports = class Tick extends EventEmitter {
+  get requestAnimationFrame() {
+    return this._raf
+  }
+
+  constructor() {
+    super(...arguments)
+
+    this._id   = null
+    this._tick = this._tick.bind(this)
+
+    try {
+      this._request = (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame
+      ).bind(window)
+      this._cancel = (
+        window.cancelAnimationFrame ||
+        window.webkitCancelAnimationFrame ||
+        window.mozCancelAnimationFrame ||
+        window.oCancelAnimationFrame ||
+        window.msCancelAnimationFrame
+      ).bind(window)
+      this._raf = true
+    } catch (e) {
+      this._request = f => setTimeout(f, 1000 / 60)
+      this._cancel  = i => clearTimeout(i)
+      this._raf     = false
+    }
+  }
+
+  on() {
+    const _ = super.on(...arguments)
+
+    if (this._id) return _
+    if (!this.listeners('tick')) return _
+
+    this._id = this._request(this._tick)
+
+    return _
+  }
+
+  off() {
+    const _ = super.off(...arguments)
+
+    if (!this._id) return _
+    if (this.listeners('tick').length) return _
+
+    this._cancel(this._id)
+    this._id = null
+
+    return _
+  }
+
+  _tick() {
+    this._id = this._request(this._tick)
+    this.emit('tick', this, new Date)
   }
 }
-
-Tick.prototype = Object.create(EventEmitter.prototype, {
-  requestAnimationFrame: {
-    get: function() { return this._raf },
-  },
-});
-
-Tick.prototype.on = function() {
-  var _ = EventEmitter.prototype.on.apply(this, Array.prototype.slice.call(arguments));
-  if (this._id) {
-    return _;
-  }
-  if (!this.listeners('tick')) {
-    return _;
-  }
-  this._id = this._request(this._onTick.bind(this));
-  return _;
-};
-
-Tick.prototype.off = function() {
-  var _ = EventEmitter.prototype.off.apply(this, Array.prototype.slice.call(arguments));
-  if (!this._id) {
-    return _;
-  }
-  if (this.listeners('tick').length) {
-    return _;
-  }
-  this._cancel(this._id);
-  this._id = null;
-  return _;
-};
-
-Tick.prototype._onTick = function() {
-  this._id = this._request(this._onTick.bind(this));
-  this.emit('tick', this, new Date);
-};
-
-module.exports = Tick;
